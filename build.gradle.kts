382ae483e6835f75a272666bfd0a5aec7949f50a
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-08-16 16:25:59 +0900

    chore: configure initial environment

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,7 @@ version = "0.0.1-SNAPSHOT"
 description = "kafka-lecture"
 
 java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17)
-    }
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 configurations {
@@ -30,13 +28,45 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter")
+
+    // 웹 애플리케이션 개발을 위한 스타터 패키지
+    // @Service 등 지원
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    // JPA
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+
+    // Kafka를 활용하기 위한 라이브러리
+    implementation("org.springframework.kafka:spring-kafka")
+    // Kafka의 메시지 및 스트림 처리
+    implementation("org.apache.kafka:kafka-streams")
+
+    // 데이터 직렬화를 위한 Avro 라이브러리
+    implementation("org.apache.avro:avro:1.11.3")
+    // Avro 데이터 다루기 위한 역/직렬화
+    implementation("io.confluent:kafka-avro-serializer:7.5.0")
+    // Avro 타입을 저장하는 SerDE를 지원하기 위한 라이브러리
+    implementation("io.confluent:kafka-streams-avro-serde:7.5.0")
+    // Avro 스키마를 저장하는 저장소
+    implementation("io.confluent:kafka-schema-registry-client:7.5.0")
+
+    // Jackson: 기본적인 역/직렬화
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // spring, java 언어를 kotlin으로 맵핑해주는 라이브러리
     implementation("org.jetbrains.kotlin:kotlin-reflect")
-    compileOnly("org.projectlombok:lombok")
-    annotationProcessor("org.projectlombok:lombok")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // 애플리케이션 모니터링 라이브러리
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+
+    // runtime에만 동작하는 h2 및 postgreSQL CDC
+    runtimeOnly("com.h2database:h2")
+    runtimeOnly("org.postgresql:postgresql")
+
     testImplementation("org.springframework.boot:spring-boot-starter-test")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
-    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("org.springframework.kafka:spring-kafka-test")
+    testImplementation(kotlin("test"))
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,9 +1,14 @@
-package org.ktor_lecture.kafkalecture
+package org.ktor_lecture
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.kafka.annotation.EnableKafka
+import org.springframework.kafka.annotation.EnableKafkaStreams
 
 @SpringBootApplication
+@EnableKafka
+// StreamsBuilderFactoryBean 활성화
+@EnableKafkaStreams
 class KafkaLectureApplication
 
 fun main(args: Array<String>) {
