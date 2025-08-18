a737027bfce38220c7958ef0e6bc400f5f216f35
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-08-18 18:45:09 +0900

    feat: settings for Consumer

---
+++ src/main/kotlin/config/KafkaConfig.kt
@@ -0,0 +1,134 @@
+package org.ktor_lecture.config
+
+import com.fasterxml.jackson.databind.deser.std.StringDeserializer
+import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
+import io.confluent.kafka.serializers.KafkaAvroDeserializer
+import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
+import org.apache.avro.generic.GenericRecord
+import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.ktor_lecture.model.OrderEvent
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
+import org.springframework.kafka.core.ConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
+import org.springframework.kafka.support.serializer.JsonDeserializer
+
+@Configuration
+class KafkaConfig {
+
+    // Broker의 URL을 받아야 함.
+    @Value("\${spring.kafka.bootstrap-servers}")
+    private lateinit var bootstrapServers: String
+
+    companion object {
+        const val SCHEMA_REGISTRY_URL = "http://localhost:8081"
+    }
+
+    @Bean
+    // 특정 도메인 객체 (OrderEvent)를 전용으로 매핑하여 타입 안정성을 보장하는 ConsumerFactory
+    // ErrorHandlingDeserializer 사용 -> Decorator 패턴
+    //      - 역직렬화 실패 시 애플리케이션 중단 방지
+    //      - 장애 격리 및 운영 안정성을 강화
+    fun orderEventConsumerFactory(): ConsumerFactory<String, OrderEvent> {
+        val props = mapOf(
+            // bootstrap server: Kafka 클라이언트(Producer, Consumer, AdminClient 등)가 Kafka 클러스터에 최초로 연결할 때 사용하는 브로커 주소 목록
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
+            ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS to StringDeserializer::class.java,
+            ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to JsonDeserializer::class.java,
+            // JSON 메시지를 OrderEvent 타입으로 우선적으로 변환
+            JsonDeserializer.VALUE_DEFAULT_TYPE to OrderEvent::class.java,
+            // 정해진 경로에 있는 패키지들을 허용 (*는 전체)
+            JsonDeserializer.TRUSTED_PACKAGES to "*",
+            // 동적인 타입을 허용하지 않음.
+            // 헤더 파싱에 대한 오버헤드를 감소시킬 수 있음.
+            JsonDeserializer.USE_TYPE_INFO_HEADERS to false,
+        )
+
+        // 아래의 ConsumerFactory를 ContainerFactory에 넣어줘야 함.
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    // @KafkaListener 기반의 메시지 수신에 적용하기 위해 사용됨.
+    fun orderEventKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, OrderEvent> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, OrderEvent>()
+        factory.consumerFactory = orderEventConsumerFactory()
+        return factory
+    }
+
+    @Bean
+    fun genericConsumerFactory(): ConsumerFactory<String, Any> {
+        val props = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
+            ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS to StringDeserializer::class.java,
+            ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS to JsonDeserializer::class.java,
+            // 정해진 경로에 있는 패키지들을 허용 (*는 전체)
+            JsonDeserializer.TRUSTED_PACKAGES to "*",
+            // 동적인 타입을 허용함.
+            JsonDeserializer.USE_TYPE_INFO_HEADERS to true,
+        )
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun genericKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, Any> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, Any>()
+        factory.consumerFactory = genericConsumerFactory()
+        return factory
+    }
+
+    @Bean
+    // CDC 값이 String으로 들어오기 때문에 Generic을 <String, String>으로 설정
+    fun cdcConsumerFactory(): ConsumerFactory<String, String> {
+        val props = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.GROUP_ID_CONFIG to "real-order-cdc-processor",
+            // 컨슈머 그룹이 특정 파티션에서 유효한 오프셋을 찾지 못했을 때, 어느 위치에서부터 읽기 시작할지를 결정하는 설정
+            // earliest: 해당 파티션의 처음 (최소 Offset)부터 읽기 시작
+            //           Commit된 Offset이 없다면 해당 토픽의 처음 메시지부터 소비
+            ConsumerConfig.AUTO_OFFSET_RESET_CONFIG to "earliest",
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            // Consumer의 Auto Commit 설정
+            ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG to true,
+            ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG to 1000,
+        )
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    fun cdcKafkaListenerContainerFactory(): ConcurrentKafkaListenerContainerFactory<String, String> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, String>()
+        factory.consumerFactory = cdcConsumerFactory()
+        return factory
+    }
+
+    @Bean
+    // CDC를 위한 컨슈머 팩토리 설정
+    fun avroConsumerFactory(): ConsumerFactory<String, GenericRecord> {
+        val props = mapOf(
+            ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
+            ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG to KafkaAvroDeserializer::class.java,
+            AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to SCHEMA_REGISTRY_URL,
+            KafkaAvroDeserializerConfig.SPECIFIC_AVRO_READER_CONFIG to false,
+            AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS to true,
+        )
+        return DefaultKafkaConsumerFactory(props)
+    }
+
+    @Bean
+    // Avro 관련 설정
+    fun avroKafkaListenerConsumerFactory(): ConcurrentKafkaListenerContainerFactory<String, GenericRecord> {
+        val factory = ConcurrentKafkaListenerContainerFactory<String, GenericRecord>()
+        factory.consumerFactory = avroConsumerFactory()
+        return factory
+    }
+}
