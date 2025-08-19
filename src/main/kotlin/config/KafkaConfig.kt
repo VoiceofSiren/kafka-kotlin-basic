10e4737bfd3581e2b2c34dc28404a6a7cf2674ae
VoiceofSiren <34666304+VoiceofSiren@users.noreply.github.com>
2025-08-19 18:35:13 +0900

    feat: settings for Producer

--- src/main/kotlin/config/KafkaConfig.kt
+++ src/main/kotlin/config/KafkaConfig.kt
@@ -4,8 +4,11 @@ import com.fasterxml.jackson.databind.deser.std.StringDeserializer
 import io.confluent.kafka.serializers.AbstractKafkaSchemaSerDeConfig
 import io.confluent.kafka.serializers.KafkaAvroDeserializer
 import io.confluent.kafka.serializers.KafkaAvroDeserializerConfig
+import io.confluent.kafka.serializers.KafkaAvroSerializer
 import org.apache.avro.generic.GenericRecord
 import org.apache.kafka.clients.consumer.ConsumerConfig
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.common.serialization.StringSerializer
 import org.ktor_lecture.model.OrderEvent
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
@@ -13,8 +16,12 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
+import org.springframework.kafka.core.DefaultKafkaProducerFactory
+import org.springframework.kafka.core.KafkaTemplate
+import org.springframework.kafka.core.ProducerFactory
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
 import org.springframework.kafka.support.serializer.JsonDeserializer
+import org.springframework.kafka.support.serializer.JsonSerializer
 
 @Configuration
 class KafkaConfig {
@@ -27,12 +34,15 @@ class KafkaConfig {
         const val SCHEMA_REGISTRY_URL = "http://localhost:8081"
     }
 
+    // -- Consumer 설정 --
+
     @Bean
     // 특정 도메인 객체 (OrderEvent)를 전용으로 매핑하여 타입 안정성을 보장하는 ConsumerFactory
     // ErrorHandlingDeserializer 사용 -> Decorator 패턴
     //      - 역직렬화 실패 시 애플리케이션 중단 방지
     //      - 장애 격리 및 운영 안정성을 강화
     fun orderEventConsumerFactory(): ConsumerFactory<String, OrderEvent> {
+        // properties 설정
         val props = mapOf(
             // bootstrap server: Kafka 클라이언트(Producer, Consumer, AdminClient 등)가 Kafka 클러스터에 최초로 연결할 때 사용하는 브로커 주소 목록
             ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
@@ -130,5 +140,47 @@ class KafkaConfig {
         val factory = ConcurrentKafkaListenerContainerFactory<String, GenericRecord>()
         factory.consumerFactory = avroConsumerFactory()
         return factory
+    }
+
+    // -- Producer 설정 --
+
+    @Bean
+    fun orderEventProducerFactory(): ProducerFactory<String, OrderEvent> {
+        val props = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to JsonSerializer::class.java,
+            JsonSerializer.ADD_TYPE_INFO_HEADERS to true,
+        )
+        return DefaultKafkaProducerFactory(props)
+    }
+
+    @Bean
+    fun kafkaTemplate(): KafkaTemplate<String, OrderEvent> {
+        return KafkaTemplate(orderEventProducerFactory())
+    }
+
+    @Bean
+    fun avroProducerFactory(): ProducerFactory<String, GenericRecord> {
+        val props = mapOf(
+            ProducerConfig.BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
+            ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG to StringSerializer::class.java,
+            ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG to KafkaAvroSerializer::class.java,
+            AbstractKafkaSchemaSerDeConfig.SCHEMA_REGISTRY_URL_CONFIG to SCHEMA_REGISTRY_URL,
+            AbstractKafkaSchemaSerDeConfig.AUTO_REGISTER_SCHEMAS to true,
+            ProducerConfig.ACKS_CONFIG to "1",  // "0": 메시지가 제대로 전송되었는지 확인하지 않음.
+                                                // "1": 메시지가 제대로 전송되었는지를 리더에게만 확인함.
+                                                // "all": 복제본이 있다면 복제본까지 확인함.
+            ProducerConfig.RETRIES_CONFIG to 3,
+            ProducerConfig.BATCH_SIZE_CONFIG to 16384,
+            ProducerConfig.LINGER_MS_CONFIG to 10,
+            ProducerConfig.COMPRESSION_TYPE_CONFIG to "snappy", // 압축률 설정
+        )
+        return DefaultKafkaProducerFactory(props)
+    }
+
+    @Bean
+    fun avroKafkaTemplate(): KafkaTemplate<String, GenericRecord> {
+        return KafkaTemplate(avroProducerFactory())
     }
 }
